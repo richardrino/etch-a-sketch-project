8418a1cee6621cf756d637f9ba6aeaa9b88431bd
Richard Rino <richardvrino@gmail.com>
2022-11-17 00:40:49 -0600

    Add feature that makes grid squares change color to blue when mouse is clicked and held over them

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
+class ClickAndHold {
+  /**
+   *
+   * @param {Object} target The HTML element to apply the event to
+   * @param {Function} callback The function to run once the target is clicked and held
+   */
+  constructor(target, callback) {
+    this.target = target;
+    this.callback = callback;
+    this.isHeld = false;
+    this.activeHoldTimeoutId = null;
+
+    ["mousedown", "touchstart"].forEach((type) => {
+      this.target.addEventListener(type, this._onHoldStart.bind(this));
+    });
+
+    ["mouseup", "touchend", "touchcancel"].forEach((type) => {
+      this.target.addEventListener(type, this._onHoldEnd.bind(this));
+    });
+
+    this.target.addEventListener("mouseover", this._onHold.bind(this));
+  }
+
+  _onHoldStart() {
+    this.isHeld = true;
+    isDrawing = true;
+
+    this.activeHoldTimeoutId = setTimeout(() => {
+      if (this.isHeld) {
+        this.callback(this.target);
+      }
+    }, 50);
+  }
+
+  _onHold() {
+    this.callback(this.target);
+  }
+
+  _onHoldEnd() {
+    this.isHeld = false;
+    isDrawing = false;
+    clearTimeout(this.activeHoldTimeoutId);
+  }
+
+  /**
+   *
+   * @param {Object} target The HTML element to apply the event to
+   * @param {Function} callback The function to run once the target is clicked and held
+   */
+  static apply(target, callback) {
+    new ClickAndHold(target, callback);
+  }
+}
+
+let isDrawing = false;
 let grid = document.querySelector(".grid");
 
+// Create default 16x16 grid
 for (let i = 1; i <= 16; i++) {
   let gridRow = document.createElement("div");
   gridRow.classList.add("grid-row");
@@ -8,6 +64,19 @@ for (let i = 1; i <= 16; i++) {
   for (let j = 1; j <= 16; j++) {
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("grid-square");
+    gridSquare.currentColor = 100;
     gridRow.appendChild(gridSquare);
+    ClickAndHold.apply(gridSquare, changeColor);
+  }
+}
+
+/**
+ *
+ * @param {Object} target The HTML element to change the background color of
+ */
+function changeColor(target) {
+  if (isDrawing) {
+    if (target.currentColor !== 0) target.currentColor -= 10;
+    target.style.backgroundColor = `rgb(0%, 0%, ${target.currentColor}%)`;
   }
 }
